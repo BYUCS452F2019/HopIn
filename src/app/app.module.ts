df6849c6dd35d1447c9e7775bc999ba128d939a7
Robbie Kelly <robbie@sskelly.net>
2019-10-07 15:25:50 -0600

    survey works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SurveyComponent } from './core';
+import { AppRouting } from './app.routes';
+import { CoreModule } from './core';
+import { MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
+const modules = [
+  AppRouting,
+  BrowserModule,
+  CoreModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  HttpClientModule,
+  HttpModule,
+  // NgbModule.forRoot(),
+];
 
 @NgModule({
-  declarations: [AppComponent, SurveyComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: modules,
   providers: [],
   bootstrap: [AppComponent],
 })
